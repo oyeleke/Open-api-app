5bf6fc7d13cd618fe88f77f8f34d5e826f766740
Ogunsola oluwatimileyn <oyelekeogunsola@gmail.com>
2020-05-02 17:18:59 +0100

    navigation and architectural setup

---
+++ app/src/main/java/com/codingwithmitch/openapi/ui/auth/LauncherFragment.kt
@@ -0,0 +1,51 @@
+package com.codingwithmitch.openapi.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.codingwithmitch.openapi.R
+import kotlinx.android.synthetic.main.fragment_launcher.*
+
+class LauncherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        login.setOnClickListener {
+            navLogin()
+        }
+
+        register.setOnClickListener {
+            navRegister()
+        }
+
+        forgot_password.setOnClickListener {
+            navForgotPassword()
+        }
+
+        focusable_view.requestFocus()
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
+    }
+
+    private fun navRegister() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+
+    private fun navForgotPassword() {
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
+}
